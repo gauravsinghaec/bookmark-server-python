8a0d648602e2b29cdf2e16f7f330f98fc3957a36
Gaurav Singh <gauravsinghaec@gmail.com>
2017-09-30 14:52:20 +0530

    Use PORT from environment.

--- bookmarkserver.py
+++ bookmarkserver.py
@@ -147,7 +147,7 @@ class Shortener(http.server.BaseHTTPRequestHandler):
 if __name__ == '__main__':
     try:    
         #port = 8000
-        port = int(os.environ.get('PORT'))
+        port = int(os.environ.get('PORT',8000))
         server_address = ('', port)
         httpd = http.server.HTTPServer(server_address, Shortener)
         print ("Web Server running on port %s" % port)
